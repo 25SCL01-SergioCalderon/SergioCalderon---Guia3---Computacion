6a4c7e12fa2e64907356500eb15d364161ede48e
25SCL01-SergioCalderon <scalderondb25@gmail.com>
2026-04-24 14:26:37 -0600

    Add files via upload

---
+++ Ejercicios/CajadeDialogoSergio.java
@@ -0,0 +1,12 @@
+package com.mycompany.cajadedialogosergio;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class CajadeDialogoSergio {
+    public static void main(String[] args) {
+        mensaje1 chejito = new mensaje1();
+        chejito.setVisible(true);
+    }
+}
